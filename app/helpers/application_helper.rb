2f4854da7cb0b1b0eb92f6a973f2249ffbdcbdb5
Prateek Obireddy <Prateek@192-168-1-5.tpgi.com.au>
2016-05-09 16:19:30 +1000

    Gravatar config added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def gravatar_for(barmen, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(barmen.email.downcase)
+    size = options[:size]
+    gravatar_url = 'http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}'
+    image_tag(gravatar_url, alt: barmen.barmenname, class: 'gravatar')
+  end
 end
