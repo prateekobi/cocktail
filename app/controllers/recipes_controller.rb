4d77b60dab94c117d3e47c39d5cbbfc93f7f9be2
Prateek Obireddy <Prateek@192-168-1-5.tpgi.com.au>
2016-05-09 18:51:49 +1000

    Update and new created

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,21 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      #do something
+      flash[:success] = "Cocktail updated successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
   private
 
     def recipe_params
