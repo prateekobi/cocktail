055f23d122f3d0aae85d21f0f317ba13406344c4
Prateek Obireddy <Prateek@192-168-1-5.tpgi.com.au>
2016-05-09 15:42:35 +1000

    recipe show page created

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
 
-  # get '/recipes', to: 'recipes#index'
-  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  # post '/recipes', to: 'recipes#create'
-  # post '/recipes/:id/edit', to: 'recipe#edit', as: 'edit_recipe'
-  # patch '/recipes/:id', to: 'recipes#update'
-  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
-  # delete '/recipes/:id', to: 'recipes#destroy'
-
-  resources :recipes 
+  resources :recipes
 end
