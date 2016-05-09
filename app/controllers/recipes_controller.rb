4883239a64957e7150c7f884044bb3f4172e6576
Prateek Obireddy <Prateek@192-168-1-5.tpgi.com.au>
2016-05-09 18:33:29 +1000

    form done for create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,25 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.barmen = Barmen.find(2);
+
+    if @recipe.save
+    flash[:success] = "Your cocktail was created successfully!"
+    redirect_to recipes_path
+    else render :new
+    end
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+
 end
